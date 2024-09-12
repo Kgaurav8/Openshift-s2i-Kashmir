a89dac443e78cc6ff3058e8b595d72f9653a1bcd
Kgaurav8 <87795172+Kgaurav8@users.noreply.github.com>
2024-09-12 09:41:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello this page has been build using S2I feature of OpenShift!"
+    return "Hello this page has been build using S2I feature of OpenShift! we have dubugged the issue facing yesterday and testing it again"
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=8080)
