6316d8ff790bd89873d3dde35fe13e70cee6a62f
Kgaurav8 <87795172+Kgaurav8@users.noreply.github.com>
2024-09-12 09:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello this page has been build using S2I feature of OpenShift! we have dubugged the issue facing yesterday and testing it again! again making changes to get the build running"
+    return "Hello this page has been build using S2I feature of OpenShift! we have dubugged the issue facing yesterday and testing it again! again making changes to get the build runnin. checking it again after adding the branch as main"
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=8080)
